0f759ace933f2d8c09e187471f585badd8019c4b
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2025-08-04 15:11:09 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 768 ms, Memory: 78616 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, List<Integer>> sortByX = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] nums = br.readLine().split(" ");
+            int x = Integer.parseInt(nums[0]);
+            int y = Integer.parseInt(nums[1]);
+            if (!sortByX.containsKey(y)) {
+                sortByX.put(y, new ArrayList<>());
+            }
+            sortByX.get(y).add(x);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, List<Integer>> elem : sortByX.entrySet()) {
+            Integer y = elem.getKey();
+            List<Integer> x = elem.getValue();
+            Collections.sort(x);
+            for (Integer getX : x) {
+                sb.append(getX);
+                sb.append(" ");
+                sb.append(y);
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
