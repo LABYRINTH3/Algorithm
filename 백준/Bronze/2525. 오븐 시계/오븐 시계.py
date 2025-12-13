dfef986a6e134b8d4ee67fd87d8d05697a1ceeee
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2025-12-14 02:04:14 +0900

    [Bronze III] Title: 오븐 시계, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,12 +1,9 @@
-A, B = map(int,input().split())
-time = int(input(""))
-timeHour = time // 60
-timeMinute = time % 60
-A = A + timeHour
-B = B + timeMinute
-if B >= 60:
-    B -= 60
-    A += 1
-if A >= 24:
-    A -= 24
-print(A, B)
+num_hour, num_minute = map(int, input().strip().split())
+num_cook_time = int(input().strip())
+[num_hour, num_minute] = [num_hour + num_cook_time // 60, num_minute + num_cook_time % 60]
+if num_minute >= 60:
+    num_hour += 1
+    num_minute -= 60
+if num_hour >= 24:
+    num_hour -= 24
+print(num_hour, num_minute)
