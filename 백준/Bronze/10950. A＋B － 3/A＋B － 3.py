25fa6fda7c86661fc75046819e93be547d6953bd
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2025-12-14 11:17:32 +0900

    [Bronze V] Title: A+B - 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,4 +1,9 @@
-case = int(input(""))
-for i in range(case):
-    a, b = map(int, input().split())
-    print(a + b)
+iter = int(input())
+
+nums = []
+for i in range(iter):
+    num1, num2 = map(int, input().strip().split(" "))
+    nums.append(num1+num2)
+
+for i in nums:
+    print(i)
