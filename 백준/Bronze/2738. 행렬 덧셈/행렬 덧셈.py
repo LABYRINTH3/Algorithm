b0457c17e21b5794c51609a2060ef2f047222152
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2025-12-29 19:20:10 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,15 @@
+matrix1 = []
+matrix2 = []
+
+N, M = map(int, input().split())
+
+for _ in range(N):
+    matrix1.append(list(map(int, input().split())))
+
+for _ in range(N):
+    matrix2.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        print(matrix1[i][j] + matrix2[i][j], end=' ')
+    print()
