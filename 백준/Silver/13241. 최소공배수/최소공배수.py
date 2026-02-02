5d49034615fa9fa9eb5a9c9ec3a66411da44fcf3
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2026-02-02 09:54:06 +0900

    [Silver V] Title: 최소공배수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,13 @@
+import sys
+
+def gcd(a: int, b: int) -> int:
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a: int, b: int) -> int:
+    return a * b // gcd(a, b)
+
+a, b = map(int, sys.stdin.readline().split())
+
+print(lcm(a, b))
