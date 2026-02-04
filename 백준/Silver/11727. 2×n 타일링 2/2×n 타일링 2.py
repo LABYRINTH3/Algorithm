1c011d14552f1c227020b3fad047ee4202c9e55f
이태훈 <156741923+LABYRINTH3@users.noreply.github.com>
2026-02-04 16:01:39 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(3)
+else:
+    a, b = 1, 3
+    for i in range(3, n+1):
+        a, b = b, b + 2*a
+    print(b%10007)
